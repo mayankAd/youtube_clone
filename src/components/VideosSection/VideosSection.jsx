e7f835d53098ee731685c860dc602f7a612c3f2a
mayankAd <blockrookie@gmail.com>
2023-10-12 12:57:43 +0530

    bug fixes

--- src/components/VideosSection/VideosSection.jsx
+++ src/components/VideosSection/VideosSection.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LandingVideosSection from "../LandingVideosSection/LandingVideosSection";
-import SearchResultsSection from "../SearchResultsSection.jsx/SearchResultsSection";
 
-const VideoSectionRouter = createBrowserRouter([
-  { path: "", Component: LandingVideosSection },
-  { path: "/search", Component: SearchResultsSection },
-]);
+
 
 const VideosSection = () => {
   return (
     <>
       <div className=" sm:ml-[235px] mt-[111px] bg-black h-[calc(100vh-112px)]">
-        <RouterProvider router={VideoSectionRouter} />
+        <LandingVideosSection/>
       </div>
     </>
   );
